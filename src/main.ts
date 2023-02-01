cf6f0afab5146ab143dfa05310d28f5c0305a162
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-02 00:47:09 +0530

    chore: Installed MDB

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
+// MDB CSS kit
+import "mdb-vue-ui-kit/css/mdb.min.css";
+
 createApp(App).mount("#app");
