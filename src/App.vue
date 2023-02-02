0816c6d79d8e393ddb3d04fe59c53463d7697015
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-03 00:30:34 +0530

    chore: Reinstated project with vite

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Home from '@/pages/Home.vue'; 
+</script>
+
+<template>
+    <Home />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from '@/App.vue'
+
+// MDB Kit
+import 'mdb-vue-ui-kit/css/mdb.min.css';
+
+const app = createApp(App)
+
+app.mount('#app')
