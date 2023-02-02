b4067f089f02783141911418cc31a6bda4d61a61
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-02 23:45:15 +0530

    fix: Removing wrong project configuration

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-
-// MDB CSS kit
-import "mdb-vue-ui-kit/css/mdb.min.css";
-
-createApp(App).mount("#app");
