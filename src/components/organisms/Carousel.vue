5fb91e1c7bde5ad35997cba0ad7f46dcef887806
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-06 23:22:52 +0530

    feat: Adds Carousel

---
+++ src/components/organisms/Carousel.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { MDBCarousel } from "mdb-vue-ui-kit";
+
+const items1 = [
+    {
+        src: "https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp",
+        alt: "...",
+        label: "First slide label",
+        caption: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        src: "https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp",
+        alt: "...",
+        label: "Second slide label",
+        caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        src: "https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp",
+        alt: "...",
+        label: "Third slide label",
+        caption:
+            "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    }
+];
+
+const carousel1 = ref(0);
+</script>
+
+<template>
+    <MDBCarousel v-model="carousel1" :items="items1" fade />
+</template>
+
+<style></style>
---
+++ src/components/organisms/Navbar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+    MDBNavbar,
+    MDBNavbarItem,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarToggler,
+    MDBBadge,
+    MDBIcon,
+    MDBCollapse
+} from 'mdb-vue-ui-kit';
+
+const collapse7 = ref(true);
+</script>
+
+<template>
+    <MDBNavbar container expand="lg" dark bg="dark" class="d-flex justify-content-between">
+        <MDBNavbarBrand>
+            <MDBIcon icon="mdb" iconStyle="fab" size="2x" />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler target="#navbarColor01" @click="collapse7 = !collapse7"></MDBNavbarToggler>
+        <MDBCollapse id="navbarColor01" v-model="collapse7">
+            <MDBNavbarNav class="mb-2 mb-lg-0">
+                <MDBNavbarItem href="#" linkClass="link-secondary">Dashboard</MDBNavbarItem>
+                <MDBNavbarItem href="#" linkClass="link-secondary">Team</MDBNavbarItem>
+                <MDBNavbarItem href="#" linkClass="link-secondary">Projects</MDBNavbarItem>
+            </MDBNavbarNav>
+        </MDBCollapse>
+    
+        <MDBNavbarNav class="mb-2 mb-lg-0 d-flex flex-row">
+            <MDBNavbarItem to="#" class="me-3 me-lg-0" linkClass="link-secondary">
+                <MDBIcon icon="shopping-cart"></MDBIcon>
+            </MDBNavbarItem>
+            <MDBNavbarItem to="#" class="me-3 me-lg-0" linkClass="link-secondary">
+                <MDBIcon icon="bell" />
+                <MDBBadge notification color="danger" pill>1</MDBBadge>
+            </MDBNavbarItem>
+            <MDBNavbarItem href="#" class="me-3 me-lg-0">
+                <img src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg" class="rounded-circle" height="22" alt=""
+                    loading="lazy" />
+            </MDBNavbarItem>
+        </MDBNavbarNav>
+    </MDBNavbar>
+</template>
+
+<style>
+
+</style>
