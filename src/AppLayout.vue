54640c726acdc30255121125e9304dd0524fe1b8
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-08 00:34:44 +0530

    feat: Created an app layout

---
+++ src/AppLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navbar from '@/components/organisms/Navbar.vue';
+import Footer from './components/organisms/Footer.vue';
+
+</script>
+<template>
+    <Navbar />
+
+    <main>
+        <slot></slot>
+    </main>
+
+    <Footer />
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,31 +2,24 @@
 
 // Atoms / Molecules / Organisms
 import Card from "@/components/molecules/Card.vue";
+import AppLayout from "@/AppLayout.vue";
 import Carousel from "@/components/organisms/Carousel.vue";
-import Navbar from "@/components/organisms/Navbar.vue";
-import Footer from "@/components/organisms/Footer.vue";
 
 </script>
 
 <template>
-    <Navbar />
-
-    <Carousel />
-    
-    <div class="container pt-2">
-        <div class="text-center">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 mb-4">
-                    <Card
-                        image="/denim_shirt.jpg"
-                        title="Text"
-                    />
+    <AppLayout>
+        <Carousel />
+        <div class="container pt-2">
+            <div class="text-center">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <Card image="/denim_shirt.jpg" title="Text" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <Footer />
+    </AppLayout>
 </template>
 
 <style>
